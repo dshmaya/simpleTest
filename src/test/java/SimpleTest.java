a35f2b3f3dc89e9eb14adb3a97e79669459a86bc
dshmaya <dshmaya@users.noreply.github.com>
2020-06-18 14:52:08 +0300

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
  
 /**  
- * Created by dshmaya on 15/11/2017 ......123456789012
+ * Created by dshmaya on 15/11/2017 ......1234567890123
  */  
 public class SimpleTest {
 	
